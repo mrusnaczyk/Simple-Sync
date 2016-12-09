7ca6f37f1da8a31fb21c5884e626856a015198f9
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2016-12-09 14:25:30 -0600

    Fixed.

--- Simple-Sync-Client/src/util/Settings.java
+++ Simple-Sync-Client/src/util/Settings.java
@@ -10,14 +10,10 @@ public class Settings {
 	public static final String domain = "cs.rusnaczyk.tk";
 	// How often the server will sync in seconds
 	public static final int syncInterval = 5;
-<<<<<<< HEAD
 	// Nothing important down here
 	public static final String secretSecret = "frUEEUzL4za4fJVSWqWz5v=sUjXe%b!V+Sv=fVx!b?GnRUHGzn"
 			+ "&_-ff%bxy?Dfb6WLJj?f@G!JjrNgJSz=+J=^WasgdcYPkDdtZGXN-U3JsZD9@p_$@4FcJRC5aJ_QyJqu4v"
 			+ "P-Vn?Q6HZ6^B8!VKj%Vc?z+FuG$uBCGE=+snR=FRt+4R$M&*E7kPffMR4hV+FKX3y-4EtR%kq@%S%sEsCv"
 			+ "2zUsGnqqsrhCq=t@uq5EMcuYN&Q=UQ!P#pedhqRzLG";
 
-=======
-	//
->>>>>>> branch 'SSL_Testing' of https://github.com/mrusnaczyk/Simple-Sync.git
 }
