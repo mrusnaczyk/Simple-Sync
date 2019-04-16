974afe216a9fc3aba8b76e0472769f15f3b010c7
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2019-04-16 18:58:02 -0500

    Update login.php

--- Simple-Sync-Server/auth/login.php
+++ Simple-Sync-Server/auth/login.php
@@ -4,11 +4,6 @@
       include '../manage/auth/conn_config.php';
 
       //Hash the password before sending to DB
-      /*
-      Side note: it wasn't until after I finished that I realized that
-      doing this was somehwat pointless because the password was still sent in the
-      clear from the form. :/ 
-      */
       $userSalt = getSalt($user);
       $passwordHash = md5($userSalt.$pass);
 
